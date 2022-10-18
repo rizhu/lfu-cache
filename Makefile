a96db03285cddc980003c9d805521a10cf3ea0cf
Richard Hu <richard.hu2001@gmail.com>
2022-10-18 13:37:18 -0700

    Test copy

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
 clean:
 	rm -rf build/
 
-test: test_kv_linked_list_basic test_lfu_cache_hashable test_lfu_cache_basic
+test: test_kv_linked_list_basic test_kv_linked_list_copy test_lfu_cache_hashable test_lfu_cache_basic
 
 test_kv_linked_list_basic:
 	mkdir -p build/
 	mkdir -p build/kv_linked_list/
 	g++ -std=c++20 -o build/kv_linked_list/test_basic tests/kv_linked_list/test_basic.cpp
 	./build/kv_linked_list/test_basic
+
+test_kv_linked_list_copy:
+	mkdir -p build/
+	mkdir -p build/kv_linked_list/
+	g++ -std=c++20 -o build/kv_linked_list/test_copy tests/kv_linked_list/test_copy.cpp
+	./build/kv_linked_list/test_copy
 
 test_lfu_cache_hashable:
 	mkdir -p build/
---
+++ tests/kv_linked_list/test_copy.cpp
@@ -0,0 +1,102 @@
+#include "../../libs/kv_linked_list.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+void test_copy_constructor() {
+    using LL = KVLinkedList<int, int>;
+    using Node_t = typename LL::Node_t;
+
+    auto ll1 = LL();
+
+    ll1.emplace_back(1, 1);
+    ll1.emplace_back(2, 2);
+    ll1.emplace_back(3, 3);
+    ll1.emplace_back(4, 4);
+
+    LL ll2(ll1);
+    for (Node_t *original = ll1.front(), *copy = ll2.front();
+        original != ll1.end() && copy != ll2.end();
+        original = original->next(), copy = copy->next())
+    {
+        assert(original->key() == copy->key());
+        assert(original->value() == copy->value());
+    }
+
+    for (Node_t *node = ll2.front(); node != ll2.end(); node = node->next()) {
+        node->set_key(node->key() - 1);
+        node->set_value(node->value() + 1);
+    }
+
+    ll1.pop_back();
+    ll2.emplace_back(4, 6);
+
+    for (Node_t *original = ll1.front(), *copy = ll2.front();
+        original != ll1.end() && copy != ll2.end();
+        original = original->next(), copy = copy->next())
+    {
+        assert(original->key() == copy->key() + 1);
+        assert(original->value() == copy->value() - 1);
+    }
+
+    assert(ll1.size() == 3);
+    assert(ll2.size() == 5);
+}
+
+void test_copy_assignment() {
+    using LL = KVLinkedList<int, int>;
+    using Node_t = typename LL::Node_t;
+
+    LL ll1 = LL();
+    ll1.emplace_back(1, 1);
+    ll1.emplace_back(2, 2);
+    ll1.emplace_back(3, 3);
+    ll1.emplace_back(4, 4);
+
+    LL ll2;
+    ll2.emplace_back(0, 0);
+    ll2.emplace_back(1, 1);
+    ll2.emplace_back(2, 2);
+    int i = 0;
+    for (Node_t *node = ll2.front(); node != ll2.end(); node = node->next()) {
+        assert(node->key() == i);
+        assert(node->value() == i);
+        i++;
+    }
+
+    ll2 = ll1;
+    for (Node_t *original = ll1.front(), *copy = ll2.front();
+        original != ll1.end() && copy != ll2.end();
+        original = original->next(), copy = copy->next())
+    {
+        assert(original->key() == copy->key());
+        assert(original->value() == copy->value());
+    }
+
+    for (Node_t *node = ll2.front(); node != ll2.end(); node = node->next()) {
+        node->set_key(node->key() - 1);
+        node->set_value(node->value() + 1);
+    }
+
+    ll1.pop_back();
+    ll2.emplace_back(4, 6);
+
+    for (Node_t *original = ll1.front(), *copy = ll2.front();
+        original != ll1.end() && copy != ll2.end();
+        original = original->next(), copy = copy->next())
+    {
+        assert(original->key() == copy->key() + 1);
+        assert(original->value() == copy->value() - 1);
+    }
+
+    assert(ll1.size() == 3);
+    assert(ll2.size() == 5);
+}
+
+int main() {
+    test_copy_constructor();
+    test_copy_assignment();
+
+    cout << "\033[1;4;32mtests/kv_linked_list/test_copy: PASSED\033[0m" << endl;
+}
